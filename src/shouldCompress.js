848a3337926854ea6979f4c5cc01d15cf3b363c8
Brandy <brandon-kah@live.com>
2021-02-01 12:13:34 +0800

    Update shouldCompress.js

--- src/shouldCompress.js
+++ src/shouldCompress.js
@@ -1,8 +1,5 @@
-const isAnimated = require('is-animated')
-
-const MIN_COMPRESS_LENGTH = process.env.MIN_COMPRESS_LENGTH || 2048
-const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 50 //~100KB
-const APNG_THRESH_LENGTH = MIN_COMPRESS_LENGTH * 100 //~200KB
+const MIN_COMPRESS_LENGTH = 1024
+const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 100 //~100KB
 
 function shouldCompress(req, buffer) {
   const { originType, originSize, webp } = req.params
@@ -16,11 +13,6 @@ function shouldCompress(req, buffer) {
     originSize < MIN_TRANSPARENT_COMPRESS_LENGTH
   ) {
     return false
-  }
-  
-  if(!process.env.DISABLE_ANIMATED && (originType.endsWith('png')) && isAnimated(buffer) && originSize < APNG_THRESH_LENGTH){
-      //It's an animated png file, let it pass through through if small enough
-      return false
   }
 
   return true
