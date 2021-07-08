246a5571ebccb926c6f9c34efdb09125bab64840
Brandy <brandon-kah@live.com>
2021-07-09 07:59:19 +0800

    Bork?

--- src/shouldCompress.js
+++ src/shouldCompress.js
@@ -5,25 +5,21 @@ const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 50 //~100KB
 const APNG_THRESH_LENGTH = MIN_COMPRESS_LENGTH * 100 //~200KB
 
 function shouldCompress(req, buffer) {
-  const { originType, originSize, webp } = req.params
+    const { originType, originSize, webp } = req.params
 
-  if (!originType.startsWith('image')) return false
-  if (originSize === 0) return false
-  if (webp && originSize < MIN_COMPRESS_LENGTH) return false
-  if (
-    !webp &&
-    (originType.endsWith('png') || originType.endsWith('gif')) &&
-    originSize < MIN_TRANSPARENT_COMPRESS_LENGTH
-  ) {
-    return false
-  }
-  
-  if(!process.env.DISABLE_ANIMATED && (originType.endsWith('png')) && isAnimated(buffer) && originSize < APNG_THRESH_LENGTH){
-      //It's an animated png file, let it pass through through if small enough
-      return false
-  }
+    if (!originType.startsWith('image')) return false
+    if (originSize === 0) return false
+    if (webp && originSize < MIN_COMPRESS_LENGTH) return false
+    if (!webp && (originType.endsWith('png') || originType.endsWith('gif')) && originSize < MIN_TRANSPARENT_COMPRESS_LENGTH) {
+        return false
+    }
+      
+    if ((originType.endsWith('png')) && isAnimated(buffer) && originSize < APNG_THRESH_LENGTH) {
+        //It's an animated png file, let it pass through through if small enough
+        return false
+    }
 
-  return true
+    return true
 }
 
 module.exports = shouldCompress
