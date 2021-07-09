47598ce35ae1dd4662d162ff8e95ce35ebfb6714
Brandy <brandon-kah@live.com>
2021-07-09 14:28:34 +0800

    Hmmm

--- src/compress.js
+++ src/compress.js
@@ -3,22 +3,21 @@ const redirect = require('./redirect')
 const isAnimated = require('is-animated')
 
 function compress(req, res, input) {
-    const format = 'webp'
     const originType = req.params.originType
 
     const image = sharp(input);
     
     image
         .metadata(function(err, metadata) {
-            sharp(input, { animated: (originType.endsWith('gif') || originType.endsWith('png') || originType.endsWith('apng')) && isAnimated(input) ? true : false })
-            .toFormat(format, { quality: 90 })
+            sharp(input, { animated: isAnimated(input) ? true : false })
+            .toFormat('webp', { quality: 90 })
             .toBuffer((err, output, info) => {
                 if (err || !info || res.headersSent) return redirect(req, res)
                    
-                if (format === 'webp' && (originType.endsWith('gif') || originType.endsWith('png') || originType.endsWith('apng')) && isAnimated(input)) console.log('Animated image converted!')
+                if (isAnimated(input)) console.log('Animated image converted!')
                     else console.log('Image converted!')
                    
-                setResponseHeaders(info, format)
+                setResponseHeaders(info, 'webp')
                 res.write(output)
                 res.end()
             })
@@ -27,7 +26,7 @@ function compress(req, res, input) {
     function setResponseHeaders (info, imgFormat){
         res.setHeader('content-type', `image/${imgFormat}`)
         res.setHeader('content-length', info.size)
-        let filename = (new URL(req.params.url).pathname.split('/').pop() || "image") + '.' + format
+        let filename = (new URL(req.params.url).pathname.split('/').pop() || "image") + '.' + 'webp'
         res.setHeader('Content-Disposition', 'inline; filename="' + filename + '"' )
         res.setHeader('x-original-size', req.params.originSize)
         res.setHeader('x-bytes-saved', req.params.originSize - info.size)
