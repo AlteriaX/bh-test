e9d409abad7760836364426cfdd0cfa35589b7c6
Brandy <brandon-kah@live.com>
2022-02-23 23:38:26 +0800

    Go back

--- src/proxy.js
+++ src/proxy.js
@@ -1,40 +1,48 @@
-const fetch = require('node-fetch');
-const pick = require('lodash').pick;
-const shouldCompress = require('./shouldCompress');
-const redirect = require('./redirect');
-const compress = require('./compress');
-const bypass = require('./bypass');
-const copyHeaders = require('./copyHeaders');
+const request = require('request')
+const pick = require('lodash').pick
+const shouldCompress = require('./shouldCompress')
+const redirect = require('./redirect')
+const compress = require('./compress')
+const bypass = require('./bypass')
+const copyHeaders = require('./copyHeaders')
 
 function proxy(req, res) {
-    fetch(
-        req.params.url,
-        {
-            gzip: true,
-            headers: {
-                ...pick(req.headers, ['cookie', 'dnt', 'referer']),
-                'user-agent': 'Bandwidth-Hero Compressor',
-                'x-forwarded-for': req.headers['x-forwarded-for'] || req.ip,
-                via: '1.1 bandwidth-hero'
-            },
-        })
-        .then(origin => {
-            if (!origin.ok) {
-                return redirect(req, res);
-            }
-            req.params.originType = origin.headers.get('content-type') || '';
-            origin.buffer().then(buffer => {
-                req.params.originSize = buffer.length;
-                copyHeaders(origin, res);
-                res.setHeader('content-encoding', 'identity');
-                if (shouldCompress(req)) {
-                    compress(req, res, buffer)
-                } else {
-                    bypass(req, res, buffer)
-                }
-            })
-        })
-        .catch(e => console.log(e));
+    var isMediaStream
+  request.get(
+    req.params.url,
+    {
+      headers: {
+        ...pick(req.headers, ['cookie', 'dnt', 'referer']),
+        'user-agent': 'Bandwidth-Hero Compressor',
+        'x-forwarded-for': req.headers['x-forwarded-for'] || req.ip,
+        via: '1.1 bandwidth-hero'
+      },
+      timeout: 10000,
+      maxRedirects: 5,
+      encoding: null,
+      strictSSL: false,
+      gzip: true,
+      jar: true
+    },
+    (err, origin, buffer) => {
+      if (err || origin.statusCode >= 400) return redirect(req, res)
+
+      if(!isMediaStream){
+        copyHeaders(origin, res)
+        res.setHeader('content-encoding', 'identity')
+        let originType = origin.headers['content-type'] || ''
+        req.params.originType = originType
+        req.params.originSize = buffer.length
+
+        if (shouldCompress(req, buffer)) {
+            isMediaStream = false
+            compress(req, res, buffer)
+        } else {
+            bypass(req, res, buffer)
+        }
+      }
+    }
+  )
 }
 
-module.exports = proxy;
+module.exports = proxy
