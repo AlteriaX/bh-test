772c0d20d0fddb30c4f7c9d261b5f9cb6408c6a3
Brandy <brandon-kah@live.com>
2022-02-23 20:38:22 +0800

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -3,35 +3,24 @@ const redirect = require('./redirect')
 const isAnimated = require('is-animated')
 
 function compress(req, res, input) {
-    const originType = req.params.originType
 
-    const image = sharp(input);
-    
-    image
-        .metadata(function(err, metadata) {
-            sharp(input, { animated: isAnimated(input) ? true : false })
-            .toFormat('webp', { quality: isAnimated(input) ? 80 : 90 })
-            .toBuffer((err, output, info) => {
-                if (err || !info || res.headersSent) return redirect(req, res)
-                   
-                if (isAnimated(input)) console.log('Animated image converted!')
-                    else console.log('Image converted!')
-                   
-                setResponseHeaders(info, 'webp')
-                res.write(output)
-                res.end()
-            })
+    sharp(input, { animated: isAnimated(input) ? true : false })
+        .toFormat('webp', { quality: isAnimated(input) ? 80 : 90 })
+        .toBuffer((err, output, info) => {
+            if (err || !info || res.headersSent) return redirect(req, res)
+
+            res.setHeader('content-type', `image/${format}`)
+            res.setHeader('content-length', info.size)
+            res.setHeader('x-original-size', req.params.originSize)
+            res.setHeader('x-bytes-saved', req.params.originSize - info.size)
+            res.status(200)
+            res.write(output)
+            res.end()
+            
+            console.log("Original: "+req.params.originSize+" | Modified: "+info.size+" | Saved: "+(req.params.originSize - info.size))
+            if (isAnimated(input)) console.log('Animated image converted!')
+              else console.log('Image converted!')
         })
-    
-    function setResponseHeaders (info, imgFormat){
-        res.setHeader('content-type', `image/${imgFormat}`)
-        res.setHeader('content-length', info.size)
-        let filename = (new URL(req.params.url).pathname.split('/').pop() || "image") + '.' + 'webp'
-        res.setHeader('Content-Disposition', 'inline; filename="' + filename + '"' )
-        res.setHeader('x-original-size', req.params.originSize)
-        res.setHeader('x-bytes-saved', req.params.originSize - info.size)
-        res.status(200)
-    }
 }
 
 module.exports = compress
