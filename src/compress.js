86d6e3f1807a0e8c74f8932e4e0c572806fef302
Brandy <brandon-kah@live.com>
2022-02-23 20:41:00 +0800

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -5,7 +5,7 @@ const isAnimated = require('is-animated')
 function compress(req, res, input) {
 
     sharp(input, { animated: isAnimated(input) ? true : false })
-        .toFormat('webp', { quality: isAnimated(input) ? 80 : 90 })
+        .toFormat('webp', { quality: 90 })
         .toBuffer((err, output, info) => {
             if (err || !info || res.headersSent) return redirect(req, res)
 
